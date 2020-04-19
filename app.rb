92665de16d3582304fbaf097d777c6150db00698
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-19 19:56:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-  @name = name.all
+  # @name = name.all
  
-  erb :'name/index.html.erb'
+  # erb :'name/index.html.erb'
 end
 end
